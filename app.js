e73d99b54185a7b9a63429dda27311d7585efae8
komaltiwari <komal55tiwari87@gmail.com>
2026-08-17 08:40:11 +0530

    Wanderlust project

--- app.js
+++ app.js
@@ -126,5 +126,8 @@ app.use((err, req, res, next) => {
 });
 
 app.listen(8080, () => {
-    console.log("server is listening to port 8080")
+console.log("server is listening to port 8080")
 });
+// app.listen(process.env.PORT || 8080, () => {
+//     console.log(`server is listening on port ${process.env.PORT || 8080}`);
+// });
